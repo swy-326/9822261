ca3e77772d0fea98c268ef4015401842f5359714
Sungwon Yoon <03judy@naver.com>
2020-11-13 23:43:44 -0300

    semana 13
    semana 13, versao limpa

--- 2020_2/IAA I/Main.java
+++ 2020_2/IAA I/Main.java
@@ -4,25 +4,26 @@ import java.io.FileNotFoundException;
 
 // https://www.codesdope.com/blog/article/backtracking-to-solve-a-rat-in-a-maze-c-java-pytho/
 
-
 public class Main {
 
     static int [][] m = new int[5][5];
     static int [][] visitou = new int[5][5]; //USADA PARA VERIFICAR SE A POSIÇÃO JÁ FOI VISITADA
 
+    // condicoes validas
     public static boolean podeAvancar(int i, int j){
         return ( i >= 0 && i <= 4 && j >= 0 && j <= 4 && m[i][j] == 0 && visitou[i][j] == 0);
     }
 
+    // funcao principal para achar solucao
     public static boolean visita(int i, int j) {
 
-        boolean temp = false;
-
+        // se chegou no final
         if (i == 4 && j == 4 && m[i][j] == 0){
             visitou[i][j] = 1;
             return true;
         } 
 
+        // se for um indice valido
         if (podeAvancar(i, j)){
 
             visitou[i][j] = 1;
@@ -31,11 +32,11 @@ public class Main {
             if( visita(i-1, j) ) return true;
             if( visita(i, j-1) ) return true;
 
+            // backtracking
             visitou[i][j] = 0;
             return false;
-
         }
-        
+    
         return false;
     } 
     
@@ -52,8 +53,9 @@ public class Main {
             boolean sucesso = false;
             visitou = new int[5][5];
 
-            sc.nextLine();
 
+            // recebe numero e atribui "nao-visitado" para matriz visistou
+            sc.nextLine();
             for(int i = 0; i < 5; i++) {
                 for(int j = 0; j < 5; j++) {
                     visitou[i][j] = 0;
@@ -61,14 +63,22 @@ public class Main {
                 }
             }
 
+
+            // achar solucao
             sucesso=visita(0, 0);
 
-            if(sucesso) {
-                System.out.println("COPS");
+            if(sucesso) System.out.println("COPS");
+            else System.out.println("ROBBERS");
+
+
+            // imprime a solucao
+            for(int i = 0; i < 5; i++) {
+                for(int j = 0; j < 5; j++) {
+                    System.out.printf("%d ", visitou[i][j]);
+                }
+                System.out.printf("\n");
             }
-            else {
-                System.out.println("ROBBERS");
-            }
+            System.out.printf("\n\n\n");
 
             cont++;
         }
