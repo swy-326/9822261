741e88c3ad7a03dd939381b7327a19136f56419b
Sungwon Yoon <03judy@naver.com>
2020-11-17 16:09:00 -0300

    Add files via upload

---
+++ 2020_2/IAA I/uri1401_sem14.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// uri 1401
+
+void swap(char* a, char* b){
+	char temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+void insertionSort(char* arr, int tam){
+	int i, j;
+	char chave;
+
+	for (i = 1; i < tam; i++){
+		chave = arr[i];
+		for(j = i - 1; j >= 0 && arr[j] > chave; j--){
+			arr[j+1] = arr[j];
+		}
+		arr[j+1] = chave;
+	}
+}
+
+
+void permutacao(char* str, int n){
+
+	if (strlen(str) == n) printf("%s\n", str);
+
+	for(int i = n; i < strlen(str); i++){
+
+	if (i >= strlen(str)-1 || str[i] != str[i+1]){
+		swap(&str[i], &str[n]);
+
+		for(int j = n+1; j <= i; j++){
+			if(str[j] > str[i]) swap(&str[j], &str[i]);
+		}
+
+		permutacao(str, n+1);
+
+		for(int j = i; j >= n+1; j--){
+			if(str[j] < str[i]) swap(&str[j], &str[i]);
+		}
+
+		swap(&str[i], &str[n]);
+
+	}
+	}
+
+}
+
+
+
+int main(){
+
+	int n; scanf("%d", &n);
+
+	for(int i = 0; i < n; i++){
+		char str[11]; scanf("%s", str);
+		insertionSort(str, strlen(str));
+		permutacao(str, 0);
+		printf("\n");
+	}
+
+	return 0;
+}
